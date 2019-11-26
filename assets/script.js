e0c4a79c7402102a2e0d6ddc325a98aec717a39d
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2019-11-26 14:10:06 +1030

    added outline for inputs

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+//header section
+//*********************************************** */
+//show current date
+
+var selectedDate = document.querySelector("#selectedDate");
+
+// variables
+var currentDate = moment().format('LL');
+
+
+
+//show current date for now
+selectedDate.textContent = currentDate;
+
+
+// ***********************************************
+//showing hourly sections
+
+//variables // not in use
+var startHour = 9;
+var endHour = 5;
+
+
+var hourArray = [9,10,11,12,1,2,3,4,5];
+
+
+
+
+$(document).ready(function(){
+
+    //
+
+})
