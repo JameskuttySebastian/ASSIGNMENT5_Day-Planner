bc1636d2df6ca61feedd3bde0ae6026e474db2cd
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2019-12-01 12:34:29 +1030

    added readme.md file

--- README.md
+++ README.md
@@ -3,10 +3,13 @@
 ## This application works as follows
 
 1. Application creates daily planner for the day
+
+![picture alt](assets/img/AppHome.PNG "Home Page")
+
 2. As per the application settings (currently it is 9 am to 5 pm), it will create sections for each hour
 3. User can key in an save tasks for each hour
 4. Depending on the hour, application will show different colour for each hour
-    -*4.1*For all the previous hours other than current hour, it will show grey background
-    -*4.2* For current hour, system will show green background
-    -*4.3* For futur hours, system will show blue background
-4. After refresh or reboot, system will show the saved tasks 
+    i. For all the previous hours other than current hour, it will show grey background
+    ii. For current hour, system will show green background
+    iii. For futur hours, system will show blue background
+5. After refresh or reboot, system will show the saved tasks 
---
+++ assets/javascript/script.js
@@ -0,0 +1,243 @@
+
+$(document).ready(function () {
+
+    //header section
+    //*********************************************** */
+    //show current date
+
+    var selectedDate = document.querySelector("#selectedDate");
+
+    // variables
+    var currentDate = moment().format('LL');
+    //show current date for now
+    selectedDate.textContent = currentDate;
+
+
+    var currentHour = parseInt(moment().format("H"));
+    // var currentHour = 12;
+    // console.log(currentHour);
+
+
+    // ***********************************************
+    //showing hourly sections
+    var hourArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    var hourArrayAMPM = ["9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm"];
+
+    dateToSave = moment().format('DD-MM-YYYY');
+
+    // dateToSave = "28-10-2019"
+
+    // console.log(dateToSave);
+
+
+    function createInputTextArea(hourArrayAMPM, hourArray) {
+        // freate textarea
+        var inputTextArea = $("<textarea>");
+        // inputTextArea.text("Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit vel quasi labore, delectus, veniam vero");
+
+        inputTextArea.addClass("form-control");
+        inputTextArea.addClass(hourArrayAMPM);
+        inputTextArea.addClass("setBackground" + hourArray);
+        inputTextArea.attr("id", "txt" + hourArray);
+        inputTextArea.attr("textarea-associated-hour", hourArray);
+        return (inputTextArea);
+    }
+
+    function createSaveButton(hourArray) {
+        var saveButton = $("<button>");
+        saveButton.addClass("btn btn-info");
+        saveButton.attr("id", "btn" + hourArray);
+        saveButton.attr("button-associated-hour", hourArray);
+
+        //creating button image span
+        var saveButtonSpan = $("<span>");
+        saveButtonSpan.addClass("fa fas fa-sticky-note");//add image for save button
+        saveButtonSpan.css("pointer-events", "none");
+
+        //append save button span to save button
+        saveButton.append(saveButtonSpan);
+
+        return (saveButton);
+    }
+
+    function createHrLine(hourArray) {
+        var hrLine = $("<hr>");
+        hrLine.addClass("thinLine");
+        hrLine.attr("id", "line" + hourArray);
+        return (hrLine);
+    }
+
+
+
+
+    var taskObjectArray = {
+        objDate: "",
+        taskArray: [{
+            hourID: 0,
+            taskItem: ""
+        }]
+    }
+
+    //Checking if the task list for today in the memory
+
+    var taskObjectArrayToDisplay = "";
+
+    var retrievedTaskObjectArray = JSON.parse(localStorage.getItem("taskObjectArray"));
+    // console.log(retrievedTaskObjectArray);
+    if (retrievedTaskObjectArray === null || retrievedTaskObjectArray.objDate != dateToSave) {
+        taskObjectArrayToDisplay = taskObjectArray;
+        taskObjectArrayToDisplay.taskArray = [];
+
+        // creating new array for the date
+        taskObjectArrayToDisplay.objDate = dateToSave;
+        for (var hr = 0; hr < hourArray.length; hr++) {
+            var taskArrayMember = {
+                hourID: 0,
+                taskItem: ""
+            }
+            taskArrayMember.hourID = hourArray[hr];
+            taskArrayMember.taskItem = "";
+            taskObjectArrayToDisplay.taskArray.push(taskArrayMember);
+            // console.log(taskObjectArrayToDisplay);
+        }
+    }
+    else {
+        taskObjectArrayToDisplay = retrievedTaskObjectArray;
+        //this need to display on page load
+    }
+
+    var setBackgroundForHour = function (hrs) {
+
+        if (hrs < currentHour) {
+            $(".setBackground" + hrs).css("background-color", "rgb(204, 201, 201)");
+        }
+        else if (currentHour === hrs) {
+            $(".setBackground" + hrs).css("background-color", "rgb(188, 245, 141)");
+        }
+        else {
+            $(".setBackground" + hrs).css("background-color", "rgb(159, 213, 245)");
+        }
+    }
+
+    var displayExistingTasks = function () {
+        if (taskObjectArrayToDisplay != "") {
+            for (it = 0; it < taskObjectArrayToDisplay.taskArray.length; it++) {
+                var stringToSearch = "#txt" + taskObjectArrayToDisplay.taskArray[it].hourID;
+                $(stringToSearch).text(taskObjectArrayToDisplay.taskArray[it].taskItem);
+            }
+        }
+    }
+
+
+    // creating all the html elements to show the daily planner
+    for (var i = 0; i < hourArray.length; i++) {
+        //main div for row
+        var mainDiv = $("<div>");
+        mainDiv.addClass("row");
+        //---------------------------------------------------------------
+        //column for hour tag
+        var hourTagColumnDiv = $("<div>");
+        hourTagColumnDiv.addClass("col-sm-2 col-md-2 col-xl-1");
+        hourTagColumnDiv.addClass("setBackground" + hourArray[i]);
+
+        //text for hour column
+        var hourTagText = $("<p>");
+        hourTagText.text(hourArrayAMPM[i]);
+
+        //append hour tag to hour column
+        hourTagColumnDiv.append(hourTagText);
+        //---------------------------------------------------------------
+        // //create column and class for textbox
+        // var textAreaAndButtonDiv = $("<div>");
+        // textAreaAndButtonDiv.addClass("col-sm-10 col-md-10 col-xl-11");
+
+        //create column and class for textbox
+        var textAreaAndButtonColumnDiv = $("<div>");
+        textAreaAndButtonColumnDiv.addClass("col-sm-10 col-md-10 col-xl-11 textAreaColumn");
+        textAreaAndButtonColumnDiv.addClass("setBackground" + hourArray[i]);
+
+        // text area and button group
+        var textAreaAndButtonGroupDiv = $("<div>");
+        textAreaAndButtonGroupDiv.addClass("input-group");
+
+        
+        var inputTextArea = createInputTextArea(hourArrayAMPM[i], hourArray[i]);
+
+        //create append button div
+        var appendButtonDiv = $("<div>");
+        appendButtonDiv.addClass("input-group-append");
+
+        //creating button
+        var saveButton = createSaveButton(hourArray[i]);
+
+        // creating hr line after element
+        var hrLine = createHrLine(hourArray[i]);
+
+        //append save button to save button span
+        appendButtonDiv.append(saveButton);
+
+        // appending text area and button div
+        textAreaAndButtonGroupDiv.append(inputTextArea, appendButtonDiv)
+
+        //add input group to column
+        textAreaAndButtonColumnDiv.append(textAreaAndButtonGroupDiv);
+
+        // append bot columns to main row div
+        mainDiv.append(hourTagColumnDiv);
+        mainDiv.append(textAreaAndButtonColumnDiv, hrLine);
+
+        //add main div to hour list
+        $(".hourList").append(mainDiv);
+        // ***********************************************
+        //showing hourly sections with different colour code
+
+        setBackgroundForHour(hourArray[i]);
+
+    }
+
+    $("#line17").hide();// hide last seperating line
+
+    //displaying existing value
+
+    displayExistingTasks();
+
+    // ***********************************************
+    //upon click, check the click from button and get the value
+    // also identiry click is from which button
+
+    $(".btn").on("click", function (event) {
+
+        if (event.target.matches("button")) {
+            var targetButton = event.target;
+            // console.log(targetButton);
+            var clickedButtonHour = $(targetButton).attr("button-associated-hour");
+            // console.log(clickedButtonHour);
+
+            //generate the text area id and display
+            var textAreaID = "#txt" + clickedButtonHour;
+            var txtContent = $(textAreaID).val();
+            console.log(textAreaID, txtContent);
+            //need to save into object
+
+            // var taskObjectArray = {
+            //     objDate: "",
+            //     taskArray: [{
+            //         hourID: 0,
+            //         taskItem: ""
+            //     }]
+            // }
+            taskObjectArrayToDisplay.objDate = dateToSave;
+            for (var t = 0; t < taskObjectArrayToDisplay.taskArray.length; t++) {
+                if (taskObjectArrayToDisplay.taskArray[t].hourID == clickedButtonHour) {
+                    var stringToSearch = "#txt" + clickedButtonHour;
+                    indexOfObjectInArray = taskObjectArrayToDisplay.taskArray[t].taskItem = $(stringToSearch).val();
+                }
+            }
+            localStorage.setItem("taskObjectArray", JSON.stringify(taskObjectArrayToDisplay));
+            // console.log(JSON.stringify(taskObjectArrayToDisplay));
+        }
+    });
+
+
+
+})
